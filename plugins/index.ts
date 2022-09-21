bd1c28a441b81be614ba816ffe930998663668a0
megatkhs <tarodino0526@gmail.com>
2022-09-22 08:42:34 +0900

    [add] @vite/plugin-vueを参考に実装を進める
    - configから作成しがち

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,3 +1,4 @@
+import { Plugin, createFilter, ViteDevServer, FilterPattern } from 'vite'
 import { vitePluginPugBuild } from './build.js'
 import { vitePluginPugServe } from './serve.js'
 import type Pug from 'pug'
@@ -12,7 +13,9 @@ type PluginSettings = {
 
 const defaultSettings: PluginSettings = {}
 
-export const vitePluginStaticSiteFramework = (userSettings?: PluginSettings) => {
+export const vitePluginStaticSiteFramework = (
+  userSettings?: PluginSettings
+) => {
   const settings: PluginSettings = {
     ...defaultSettings,
     ...userSettings,
@@ -32,4 +35,65 @@ export const vitePluginStaticSiteFramework = (userSettings?: PluginSettings) => 
       reload,
     }),
   ]
+}
+
+export type Options = {
+  isProduction?: boolean
+
+  /** pugのオプション */
+  pug?: {
+    options?: Pug.Options
+    locals?: Pug.LocalsObject
+  }
+  /** pagesとする範囲 */
+  pages?:
+    | {
+        include: FilterPattern
+        exclude: FilterPattern
+      }
+    | FilterPattern
+}
+
+export type ResolvedOptions = Options & {
+  root: string
+  devServer?: ViteDevServer
+  isProduction: boolean
+}
+
+export const staticSitePlugin = (rawOptions: Options = {}): Plugin => {
+  const { pug = {}, pages = 'src/pages/**.pug', isProduction } = rawOptions
+
+  const filterArgs =
+    pages &&
+    typeof pages === 'object' &&
+    !(pages instanceof Array) &&
+    !(pages instanceof RegExp)
+      ? [pages.include, pages.exclude]
+      : [pages]
+
+  const filter = createFilter(...filterArgs)
+
+  let options: ResolvedOptions = {
+    isProduction: process.env.NODE_ENV === 'production',
+    ...rawOptions,
+    pug,
+    pages,
+    root: process.cwd(),
+  }
+
+  return {
+    name: 'vite-plugin-static-site',
+
+    configResolved(config) {
+      options = {
+        ...options,
+        root: config.root,
+        isProduction: config.isProduction,
+      }
+    },
+
+    configureServer(server) {
+      options.devServer = server
+    },
+  }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,27 @@ import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import Inspect from 'vite-plugin-inspect'
 import ViteRestart from 'vite-plugin-restart'
-import { vitePluginStaticSiteFramework } from './plugins'
+import { staticSitePlugin, vitePluginStaticSiteFramework } from './plugins'
 
 export default defineConfig({
   // ...
   root: 'src',
   build: {
-    outDir: resolve(__dirname, "dist"),
+    outDir: resolve(__dirname, 'dist'),
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src", "index.pug"),
+        main: resolve(__dirname, 'src', 'index.pug'),
       },
     },
   },
   plugins: [
     Inspect(),
+    staticSitePlugin(),
     vitePluginStaticSiteFramework({
       buildOptions: {
-        pretty: true
-      }
+        pretty: true,
+      },
     }),
-  ]
+  ],
   // ...
 })
